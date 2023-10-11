0e3f3fa41fc0b4e7e732fce8e9809d59b7a79401
Raj-Surase <rajsurase3@gmail.com>
2023-10-12 00:56:22 +0530

    frontend 80% dome

---
+++ JavaScript/back.js
@@ -0,0 +1,11 @@
+function goBack() {
+    const previousPage = localStorage.getItem('previousPage');
+    if (previousPage) {
+        // Redirect to the previous page
+        window.location.href = previousPage;
+    } else {
+        // Handle the case when there is no previous page
+        // For example, return to the app's main page
+        window.location.href = 'index.html'; // Replace with your main page URL
+    }
+}
